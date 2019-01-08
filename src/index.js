6ee7083d3fc5bd70410c7085168e32c1974344cc
moskowitza <moskowitzaaron@mac.com>
2019-01-08 16:15:32 -0500

    working with envs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from "react";
 import { render } from "react-dom";
 import Router from "./components/Router";
 import "./css/style.css";
-require("dotenv").config();
 
 render(<Router />, document.querySelector("#root"));
